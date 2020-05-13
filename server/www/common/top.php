591170299b9cd7ad03a259244abb99cd653987bb
vxggangxv <vxggangxv@naver.com>
2020-05-13 15:44:51 +0900

    edit event

--- server/www/common/top.php
+++ server/www/common/top.php
@@ -11,14 +11,14 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>에어스카이호텔</title>
     <meta name="title" content="에어스카이호텔">
-    <meta name="description" content="">
+    <meta name="description" content="인천 중구 영종도, 인천국제공항 인근 호텔, 인천차이나타운, 을왕리해수욕장, 월미도, 송도센트럴파크, 인천가볼만한곳">
     <meta name="keywords" content="에어스카이호텔">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
+	<meta name="naver-site-verification" content="ce542237c4c5c9e80418daa1900e8af5c25418a7" /> 
     <meta property="og:type" content="website">
     <meta property="og:title" content="에어스카이호텔">
-    <meta property="og:description" content="">
+    <meta property="og:description" content="인천 중구 영종도, 인천국제공항 인근 호텔, 인천차이나타운, 을왕리해수욕장, 월미도, 송도센트럴파크, 인천가볼만한곳">
     <meta property="og:image" content="<?=base_img?>/svc/img/sample/sns_thumb.jpg">
 
     <!-- <link rel="stylesheet" type="text/css" href="<?=base_static?>/svc/common/font/nanumgothic/nanumgothic.css" /> -->
@@ -110,8 +110,8 @@
                         <div class="nav-tit hidden-mdlg">
                             <img src="<?=base_img?>/svc/img/symbol/m_top_logo_w.png" alt="에어스카이호텔" class="logo-menu"><span class="i-x" onclick=""></span>
                             <div class="menu-btn-box">
-                                <a href="../reservation/" class="btn-bk">객실예약</a>
-                                <a href="../reservation/confirm.php" class="btn-bkc">예약확인</a>
+                                <a href="/reserve" class="btn-bk">객실예약</a>
+                                <a href="/history/?dd=Sws5TzdVKg==" class="btn-bkc">예약확인</a>
                             </div>
                         </div>
                         <ul class="cf menu">
@@ -128,15 +128,15 @@
                                 <a href="#none" class="title">객실안내</a>
                                 <!-- 190719 메뉴 수정 -->
                                 <ul class="sub">
-                                    <li><a href="/rooms/detail_new/std_db.php">스탠다드 더블</a></li>
-                                    <li><a href="/rooms/detail_new/std_tw.php">스탠다드 트윈</a></li>
-                                    <li><a href="/rooms/detail_new/dx_oc_db.php">디럭스 오션 더블</a></li>
-                                    <li><a href="/rooms/detail_new/dx_oc_tw.php">디럭스 오션 트윈</a></li>
-                                    <li><a href="/rooms/detail_new/dx_oc_ondol.php">디럭스 오션 온돌</a></li>
-                                    <li><a href="/rooms/detail_new/pr_oc_db.php">프리미어 오션 더블</a></li>
-                                    <li><a href="/rooms/detail_new/pr_oc_tw.php">프리미어 오션 트윈</a></li>
-                                    <li><a href="/rooms/detail_new/pr_oc_famtw.php">프리미어 오션 패밀리 트윈</a></li>
-                                    <li><a href="/rooms/detail_new/suite.php">스위트</a></li>
+                                    <li><a href="/rooms/?r=std_db">스탠다드 더블</a></li>
+                                    <li><a href="/rooms/?r=std_tw">스탠다드 트윈</a></li>
+                                    <li><a href="/rooms/?r=dx_oc_db">디럭스 오션 더블</a></li>
+                                    <li><a href="/rooms/?r=dx_oc_tw">디럭스 오션 트윈</a></li>
+                                    <li><a href="/rooms/?r=dx_oc_ondol">디럭스 오션 온돌</a></li>
+                                    <li><a href="/rooms/?r=pr_oc_db">프리미어 오션 더블</a></li>
+                                    <li><a href="/rooms/?r=pr_oc_tw">프리미어 오션 트윈</a></li>
+                                    <li><a href="/rooms/?r=pr_oc_famtw">프리미어 오션 패밀리 트윈</a></li>
+                                    <li><a href="/rooms/?r=suite">스위트</a></li>
                                 </ul>
                             </li>
                             <li>
@@ -153,10 +153,10 @@
                                 <a href="/group" class="title">단체문의</a>
                             </li>
                             <li>
-                                <a href="javascript:;" onclick="alert('준비중입니다.')" class="title">장기/단기 숙박</a>
+                                <a href="/rent" class="title">장기/단기 숙박</a>
                             </li>
                             <li>
-                                <a href="javascript:;" onclick="alert('준비중입니다.')" class="title">이벤트/축제</a>
+                                <a href="/event" class="title">이벤트/축제</a>
                             </li>
                         </ul>
                     </div>
@@ -167,8 +167,8 @@
                         <li class="item item-1 fst"><a href="/reserve">객실예약</a></li>
                         <li class="item item-2"><a href="/history/?dd=Sws5TzdVKg==">예약확인</a></li>
                         <li class="item item-3"><a href="/group">단체예약</a></li>
-                        <!-- <li class="item item-4"><a href="/rent">장기/단기예약</a></li> -->
-                        <li class="item item-4"><a href="javascript:;" onclick="alert('준비중입니다.')">장기/단기예약</a></li>
+                        <li class="item item-4"><a href="/rent">장기/단기예약</a></li>
+                        <!-- <li class="item item-4"><a href="javascript:;" onclick="alert('준비중입니다.')">장기/단기예약</a></li> -->
                     </ul>
                 </div>
             </div>
